9d78bddd5535b3ded89dc7599fc101f43db561b5
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-06-28 14:11:06 -0400

    [Workflow] Use new draw/dependency-injection component

---
+++ DependencyInjection/WorkflowIntegration.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Draw\Component\Workflow\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\IntegrationTrait;
+use Draw\Component\Security\Core\Security;
+use Draw\Component\Workflow\EventListener\AddTransitionNameToContextListener;
+use Draw\Component\Workflow\EventListener\AddUserToContextListener;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+class WorkflowIntegration implements IntegrationInterface
+{
+    use IntegrationTrait;
+
+    public function getConfigSectionName(): string
+    {
+        return 'workflow';
+    }
+
+    public function load(array $config, PhpFileLoader $loader, ContainerBuilder $container): void
+    {
+        $this->registerClasses(
+            $loader,
+            $namespace = 'Draw\\Component\\Workflow\\',
+            \dirname(
+                (new \ReflectionClass(AddTransitionNameToContextListener::class))->getFileName(),
+                2
+            ),
+        );
+
+        if (!class_exists(Security::class)) {
+            $container->removeDefinition(AddUserToContextListener::class);
+        }
+
+        $this->renameDefinitions(
+            $container,
+            $namespace,
+            'draw.workflow.'
+        );
+    }
+
+    public function addConfiguration(ArrayNodeDefinition $node): void
+    {
+        // nothing to do
+    }
+}
---
+++ Tests/DependencyInjection/WorkflowIntegrationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Draw\Component\Workflow\Tests\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\Test\IntegrationTestCase;
+use Draw\Component\DependencyInjection\Integration\Test\ServiceConfiguration;
+use Draw\Component\Workflow\DependencyInjection\WorkflowIntegration;
+use Draw\Component\Workflow\EventListener\AddTransitionNameToContextListener;
+use Draw\Component\Workflow\EventListener\AddUserToContextListener;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @property WorkflowIntegration $integration
+ */
+#[CoversClass(WorkflowIntegration::class)]
+class WorkflowIntegrationTest extends IntegrationTestCase
+{
+    public function createIntegration(): IntegrationInterface
+    {
+        return new WorkflowIntegration();
+    }
+
+    public function getConfigurationSectionName(): string
+    {
+        return 'workflow';
+    }
+
+    public function getDefaultConfiguration(): array
+    {
+        return [];
+    }
+
+    public static function provideTestLoad(): iterable
+    {
+        yield [
+            [],
+            [
+                new ServiceConfiguration(
+                    'draw.workflow.event_listener.add_user_to_context_listener',
+                    [
+                        AddUserToContextListener::class,
+                    ]
+                ),
+                new ServiceConfiguration(
+                    'draw.workflow.event_listener.add_transition_name_to_context_listener',
+                    [
+                        AddTransitionNameToContextListener::class,
+                    ]
+                ),
+            ],
+        ];
+    }
+}
